10fde79703d9e56f025ec0c48664a1b66e2ed02c
CML2 <cml2@CML2s-iMac.local>
2019-03-21 19:12:25 -0700

    first commit

--- sample_exp/database_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-	I think the code below is written by Jason Carpenter.
-	GitHub: https://github.com/jmcarpenter2
-*/
-$dbc = mysql_connect("localhost", "<databaseLogin>", "<databasePassword>"); 
-mysql_select_db("<databaseName>", $dbc);
-
-?>
